57705881f6436217c7989cf6305871e46c0f81d9
Pranav Vishal <pvishal@uwaterloo.ca>
2023-07-12 22:31:03 -0400

    transform.Translate in update

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,12 +7,12 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0.01f, 0f, 0f);
     }
 }
