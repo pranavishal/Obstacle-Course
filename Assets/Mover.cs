338f540192e3769a64a3b2135e9b0e674e6ed32e
Pranav Vishal <pvishal@uwaterloo.ca>
2023-07-22 17:25:23 -0400

    added SerializeField to variables in mover

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
