fc05ec71a359967ff2550bbc2d91a44590669b1e
Pranav Vishal <pvishal@uwaterloo.ca>
2023-07-23 13:01:54 -0400

    GetAxis horizontal and vertical

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +14,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue * 1f, 0, zValue * 1f);
     }
 }
